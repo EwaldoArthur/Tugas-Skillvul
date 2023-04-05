af91d325b510588a5029dd7ca426ec52115536ef
Albert Ewaldo <ewaldoalbert90@gmail.com>
2023-04-05 16:19:57 +0700

    first commit

---
+++ dist/script.js
@@ -0,0 +1,30 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+    menuNav.classList.toggle("menu-active");
+});
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    console.log("window.scrollY");
+    const windowPosition = window.scrollY > 0;
+    navBar.classList.toggle("scrolling-active", windowPosition);
+    menuNav.classList.remove("menu-active");
+});
+
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+      duration: 1000,
+      step: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      },
+      complete: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      }
+    });
+});
